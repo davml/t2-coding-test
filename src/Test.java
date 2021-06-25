256a9402b1c0ab8022f61a5e464df304b6d1a34d
gvb17187 <david.lewis.2017@uni.strath.ac.uk>
2021-06-25 14:25:08 +0100

    Iteration exercise

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+    public static void main(String[] args){
+        for(int i=0; i<100; i+=7){ //Multiples of 7 up until 100
+            System.out.println(i);
+        }
+
+        for(int i=0; i<200; i+=8){ //Multiples of 8 up until 200
+            System.out.println(i);
+        }
+
+        for(int i=0; i<300; i+=9){ //Multiples of 9 up until 300
+            System.out.println(i);
+        }
+    }
+}
